b5e35656996b20646dc20b158a9c55ac3a279c9b
himanshusandha <himanshupatelj007@gmail.com>
2020-10-12 22:28:00 +0530

    Updated UI for Intro.js component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/components/Intro.css";
 import avtar from "../resources/avtar.png";
 
 function Intro() {
+  const exploringString = [
+    <code style={{ color: "var(--secondaryTextColor)" }}>
+      <div className="intro_content_lang_tag_up">{"JS"}</div>
+      {"{ Javascript() => {} }"}
+    </code>,
+    <code style={{ color: "var(--secondaryTextColor)" }}>
+      <div className="intro_content_lang_tag_up">{"useState"}</div>
+      {"< React /> ⚛️"}
+    </code>,
+    <code style={{ color: "var(--secondaryTextColor)" }}>
+      <div className="intro_content_lang_tag_up">{"native"}</div>
+      {"< React Native /> ⚛️📱"}
+    </code>,
+    <code style={{ color: "var(--secondaryTextColor)" }}>
+      <div className="intro_content_lang_tag_up">{"schema"}</div>
+      {"{ GraphQL } ✡️"}
+    </code>,
+    <code style={{ color: "var(--secondaryTextColor)" }}>
+      <div className="intro_content_lang_tag_up">{"server"}</div>
+      {"< Node.js /> 🌐"}
+    </code>,
+  ];
+
+  const [activeString, setActiveString] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveString((activeString + 1) % exploringString.length);
+    }, 2500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeString, exploringString]);
+
   return (
     <React.Fragment>
       <div className="intro_base">
         <div className="intro_content">
-          <h1>
+          <div className="intro_content_name">
+            <div className="intro_content_tag_up">{"<h1>"}</div>
             Hello{" "}
             <span role="img" aria-label="wavingHand">
               👋
@@ -15,21 +50,25 @@ function Intro() {
             I'm
             <br />
             Himanshu Sandha
-          </h1>
-          {"<Web Developer />"}
-          <br />
-          From India{" "}
-          <span role="img" aria-label="flagIndian">
-            🇮🇳
-          </span>
-          <br />
-          exploring Javascript, React, React Native, GraphQL, Node.js
-          <br />
-          Founder @fregmaa
-          <br />
-          Ex TCS'er
-          <br />
-          himanshujsandha@gmail.com
+            <div className="intro_content_tag_down">{"</h1>"}</div>
+          </div>
+          <div className="intro_content_tech">{"<Web Developer />"}</div>
+          <div className="intro_content_location">
+            From India{" "}
+            <span role="img" aria-label="flagIndian">
+              🇮🇳
+            </span>
+          </div>
+          <div className="intro_content_lang">
+            exploring <br />
+            {exploringString[activeString]}
+          </div>
+          <div className="intro_content_company">
+            Founder @fregmaa
+            <br />
+            Ex TCS'er
+          </div>
+          <div className="intro_content_mail">himanshujsandha@gmail.com</div>
         </div>
         <img src={avtar} alt="avtar" className="intro_avtar" />
       </div>
